72a955b27977a6cbfdcfd2084c2cdf9be6e9ada1
Wallison Martins <wallisonpinheiro1@gmail.com>
2020-01-16 00:05:35 -0300

    first commit

---
+++ src/pages/Book/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Alert, AsyncStorage, Text, TextInput, TouchableOpacity } from 'react-native';
+
+import api from '../../services/api';
+
+import styles from './styles';
+
+export default function Book({ navigation }) {
+  const [date, setDate] = useState('');
+  const id = navigation.getParam('id');
+
+  async function handleSubmit() {
+    const user_id = await AsyncStorage.getItem('user');
+
+    await api.post(`/spots/${id}/bookings`, {
+      date
+    }, {
+      header: { user_id }
+    })
+
+    Alert.alert('Solicitação de reserva enviada.');
+
+    navigation.navigate('List');
+  }
+
+  function handleCancel() {
+    navigation.navigate('List');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERSSE *</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
---
+++ src/pages/List/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Text, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../../components/SpotList';
+
+import logo from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech} /> )}
+      </ScrollView>
+      
+    </SafeAreaView>
+  )
+}
